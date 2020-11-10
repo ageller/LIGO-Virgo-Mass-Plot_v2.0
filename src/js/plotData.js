0695d997b061885a20004be3aff515315102ca95
Aaron Geller <a-geller@northwestern.edu>
2020-11-10 13:25:33 -0600

    added 'special' sources with ?s'

--- src/js/plotData.js
+++ src/js/plotData.js
@@ -64,10 +64,10 @@ function drawArrow(pts){
 	var dist = Math.abs(pts[1].y - pts[0].y);
 
 	//tail
-	context.moveTo(pts[0].x - params.arrowThickBottom - params.arrowCurveTail,    pts[1].y + params.arrowHeadStart);
-	context.quadraticCurveTo(pts[0].x - params.arrowThickBottom, pts[1].y + dist/2., pts[0].x - params.arrowThickBottom, pts[0].y);
-	context.lineTo(pts[0].x + params.arrowThickBottom, pts[0].y);
-	context.quadraticCurveTo(pts[0].x + params.arrowThickBottom, pts[1].y + dist/2., pts[0].x + params.arrowThickBottom + params.arrowCurveTail, pts[1].y + params.arrowHeadStart);
+	context.moveTo(pts[0].x - params.arrowThickBottom - params.arrowCurveTail,    pts[1].y + params.arrowHeadStart + pts[1].r);
+	context.quadraticCurveTo(pts[0].x - params.arrowThickBottom, pts[1].y + dist/2. + pts[1].r, pts[0].x - params.arrowThickBottom, pts[0].y - pts[0].r);
+	context.lineTo(pts[0].x + params.arrowThickBottom, pts[0].y - pts[0].r);
+	context.quadraticCurveTo(pts[0].x + params.arrowThickBottom, pts[1].y + dist/2. + pts[1].r, pts[0].x + params.arrowThickBottom + params.arrowCurveTail, pts[1].y + params.arrowHeadStart + pts[1].r);
 
 	//head
 	var theta0 = 0.25*Math.PI;
@@ -75,11 +75,11 @@ function drawArrow(pts){
 	var rad = x1/Math.cos(theta0);
 	var y1 = rad*Math.sin(theta0);
 	var y2 = rad - y1;
-	context.moveTo(pts[0].x + params.arrowThickBottom + params.arrowCurveTail + params.arrowThickTop,    pts[1].y + params.arrowHeadStart + y2);
-	context.lineTo(pts[0].x, pts[1].y);
-	context.lineTo(pts[0].x - params.arrowThickBottom - params.arrowCurveTail - params.arrowThickTop,    pts[1].y + params.arrowHeadStart + y2);
+	context.moveTo(pts[0].x + params.arrowThickBottom + params.arrowCurveTail + params.arrowThickTop,    pts[1].y + params.arrowHeadStart + y2 + pts[1].r);
+	context.lineTo(pts[0].x, pts[1].y + pts[1].r);
+	context.lineTo(pts[0].x - params.arrowThickBottom - params.arrowCurveTail - params.arrowThickTop,    pts[1].y + params.arrowHeadStart + y2 + pts[1].r);
 
-	context.arc(pts[0].x, pts[1].y + params.arrowHeadStart + rad, rad, Math.PI + theta0, 2.*Math.PI - theta0);
+	context.arc(pts[0].x, pts[1].y + params.arrowHeadStart + rad + pts[1].r, rad, Math.PI + theta0, 2.*Math.PI - theta0);
 
 	//context.lineTo(pts[0].x + params.arrowThickBottom + params.arrowCurveTail + params.arrowThickTop,    pts[1].y + params.arrowHeadStart);
 	//context.quadraticCurveTo(pts[0].x + params.arrowThickBottom + params.arrowCurveTail + params.arrowThickTop/2.,  pts[1].y + 0.8*params.arrowHeadStart, pts[0].x + params.arrowThickBottom + params.arrowCurveTail + params.arrowThickTop,    pts[1].y + params.arrowHeadStart);
@@ -98,6 +98,19 @@ function drawArrow(pts){
 	return context + "";
 };
 
+function mouseOver(){
+	var name = d3.select(this).attr('class').split(" ")[0];
+	d3.selectAll('.arrow.'+name).transition().duration(200).style("opacity",1)
+	d3.selectAll('.dot.'+name).transition().duration(200).style("fill-opacity",1)
+	d3.selectAll('.'+name).classed("inFront",true);
+}
+function mouseOut(){
+	var name = d3.select(this).attr('class').split(" ")[0];
+	d3.selectAll('.arrow.'+name).transition().duration(200).style("opacity",params.opArrow)
+	d3.selectAll('.dot.'+name).transition().duration(200).style("fill-opacity",params.opMass)
+	d3.selectAll('.'+name).classed("inFront",false);
+}
+
 function populatePlot(){
 //add the data to the plot
 	console.log('populating plot ...');
@@ -112,13 +125,25 @@ function populatePlot(){
 		var x = +d.index/xNorm*params.xAxisScale.domain()[1];
 
 		var y0 = +d.mass_2_source;
-		if (y0 == null || y0 == 0) y0 = +d.mass_1_source;
+		var r0 = +d.mass_2_source;
+		if (y0 == null || y0 == 0) {
+			y0 = +d.mass_1_source;
+			r0 = +d.mass_1_source;
+		}
 
 		var y1 = +d.final_mass_source;
-		if (y1 == null || y1 == 0) y1 = +d.mass_1_source;
+		var r1 = +d.final_mass_source;
+		if (y1 == null || y1 == 0) {
+			var y1 = +d.total_mass_source;
+			var r1 = +d.total_mass_source;
+		}
+		if (y1 == null || y1 == 0) {
+			y1 = +d.mass_1_source;
+			r0 = +d.mass_1_source;
+		}
 
-		var lne = [{'x':params.xAxisScale(x),'y':params.yAxisScale(y0)},
-				   {'x':params.xAxisScale(x),'y':params.yAxisScale(y1)}];
+		var lne = [{'x':params.xAxisScale(x),'y':params.yAxisScale(y0),'r':params.radiusScale(r0)},
+				   {'x':params.xAxisScale(x),'y':params.yAxisScale(y1),'r':params.radiusScale(r1)}];
 		var path = drawArrow(lne);
 		params.SVG.append("path")
 			.attr("class",d.commonName + " arrow")
@@ -127,18 +152,10 @@ function populatePlot(){
 			.attr('fill', 'white')	
 			//.attr('fill', 'none')	
 			.style("opacity",0.5)
+			.style("cursor", "pointer")
 			.attr("d", path.toString())
-			.on('mouseover',function(){
-				var name = d3.select(this).attr('class').split(" ")[0];
-				d3.selectAll('.'+name).transition().duration(200).style("opacity",1)
-				d3.selectAll('.'+name).classed("inFront",true);
-			})
-			.on('mouseout',function(){
-				var name = d3.select(this).attr('class').split(" ")[0];
-				d3.selectAll('.arrow.'+name).transition().duration(200).style("opacity",params.opArrow)
-				d3.selectAll('.dot.'+name).transition().duration(200).style("opacity",params.opMass)
-				d3.selectAll('.'+name).classed("inFront",false);
-			})
+			.on('mouseover',mouseOver)
+			.on('mouseout',mouseOut)
 
 	}	
 
@@ -146,36 +163,84 @@ function populatePlot(){
 	//add all the dots for all the masses
 	var co = '#00BFFF'
 	params.SVG.selectAll(".dot.mf")
-		.data(params.data).enter()
-		.append("circle").filter(function(d) { return d.final_mass_source != null })
+		.data(params.data).enter().filter(function(d) { return d.final_mass_source != null })
+		.append("circle")
 			.attr("class", function(d){return d.commonName + " dot mf ";})
 			.attr("r", function(d){return params.radiusScale(+d.final_mass_source)})
 			.attr("cx", function(d) {return params.xAxisScale(+(d.index/xNorm*params.xAxisScale.domain()[1])); })
 			.attr("cy", function(d) {return params.yAxisScale(+d.final_mass_source); })
 			.style("fill", co)
-			.style("opacity",params.opMass)
+			.style("fill-opacity",params.opMass)
+			.style("stroke", co)
+			.style("stroke-opacity", 1)
+			.style("stroke-width", 2)
+			.style("cursor", "pointer")
+			.on('mouseover',mouseOver)
+			.on('mouseout',mouseOut);
 
 	params.SVG.selectAll(".dot.m1")
-		.data(params.data).enter()
-		.append("circle").filter(function(d) { return d.mass_1_source != null })
+		.data(params.data).enter().filter(function(d) { return d.mass_1_source != null })
+		.append("circle")
 			.attr("class", function(d){return d.commonName + " dot m1";})
 			.attr("r", function(d){return params.radiusScale(+d.mass_1_source)})
 			.attr("cx", function(d) {return params.xAxisScale(+d.index/xNorm*params.xAxisScale.domain()[1]); })
 			.attr("cy", function(d) {return params.yAxisScale(+d.mass_1_source); })
-			//.style("fill", 'red')
 			.style("fill", co)
-			.style("opacity",params.opMass)
+			.style("fill-opacity",params.opMass)
+			.style("stroke", co)
+			.style("stroke-opacity", 1)
+			.style("stroke-width", 2)
+			.style("cursor", "pointer")
+			.on('mouseover',mouseOver)
+			.on('mouseout',mouseOut);
 
 	params.SVG.selectAll(".dot.m2")
-		.data(params.data).enter()
-		.append("circle").filter(function(d) { return d.mass_2_source != null })
+		.data(params.data).enter().filter(function(d) { return d.mass_2_source != null })
+		.append("circle")
 			.attr("class", function(d){return d.commonName + " dot m2";})
 			.attr("r", function(d){return params.radiusScale(+d.mass_2_source)})
 			.attr("cx", function(d) {return params.xAxisScale(+d.index/xNorm*params.xAxisScale.domain()[1]); })
 			.attr("cy", function(d) {return params.yAxisScale(+d.mass_2_source); })
-			//.style("fill", 'orange')
 			.style("fill", co)
-			.style("opacity",params.opMass);
+			.style("fill-opacity",params.opMass)
+			.style("stroke", co)
+			.style("stroke-opacity", 1)
+			.style("stroke-width", 2)
+			.style("cursor", "pointer")
+			.on('mouseover',mouseOver)
+			.on('mouseout',mouseOut);
+
+	//add any without final masses?
+	params.SVG.selectAll(".dot.mf.no_final_mass")
+		.data(params.data).enter().filter(function(d) { return d.final_mass_source == null && d.total_mass_source != null})
+		.append("circle")
+			.attr("class", function(d){return d.commonName + " dot mf no_final_mass";})
+			.attr("r", function(d){return params.radiusScale(+d.total_mass_source)})
+			.attr("cx", function(d) {return params.xAxisScale(+(d.index/xNorm*params.xAxisScale.domain()[1])); })
+			.attr("cy", function(d) {return params.yAxisScale(+d.total_mass_source); })
+			.style("fill", co)
+			.style("fill-opacity",params.opMass)
+			.style("stroke", co)
+			.style("stroke-opacity", 1)
+			.style("stroke-width", 2)
+			.style("cursor", "pointer")
+			.on('mouseover',mouseOver)
+			.on('mouseout',mouseOut)
+
+	params.SVG.selectAll(".text.mf.no_final_mass")
+		.data(params.data).enter().filter(function(d) {return d.final_mass_source == null && d.total_mass_source != null})	
+		.append("text")		
+			.attr("class", function(d){return d.commonName + " text mf no_final_mass";})
+			.attr("x", function(d) {return params.xAxisScale(+(d.index/xNorm*params.xAxisScale.domain()[1])); })
+			.attr("y", function(d) {return params.yAxisScale(+d.total_mass_source) + 0.75*params.radiusScale(+d.total_mass_source); })
+			.style('fill','white')
+			.style('font-family',"sans-serif")
+			.style('font-size',1.5*params.radiusScale(+d.total_mass_source)+"px")
+			.style("text-anchor", "middle")
+			.style("cursor", "pointer")
+			.text("?")
+			.on('mouseover',mouseOver)
+			.on('mouseout',mouseOut)
 }
 
 
