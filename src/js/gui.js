e2caac8864d50dbec33fb5d2b4f2d3afce983f07
Aaron Geller <a-geller@northwestern.edu>
2021-09-30 13:24:10 -0500

    new snapshot util

--- src/js/gui.js
+++ src/js/gui.js
@@ -692,157 +692,18 @@ function changeAspect(){
 	d3.select('#renderY').attr('placeholder',params.renderY);
 }
 
-//http://bl.ocks.org/Rokotyan/0556f8facbaf344507cdc45dc3622177
-//render an image from the SVG element
-//in Frank's version I was able to make this simply scale the image, but here it seems like I need to redraw.  I wonder why??
+//https://github.com/exupero/saveSvgAsPng
+//see also here: https://spin.atomicobject.com/2014/01/21/convert-svg-to-png/
 function renderToImage(){
 	
-	var saveSVGscale = params.SVGscale;
-	var saveControlsX = params.controlsX;
+	//set the scale for the render
+	var scaleX = 1;
+	var scaleY = 1;
+	if (!isNaN(params.renderX)) scaleX = parseFloat(params.renderX)/parseFloat(params.SVG.style('width'));
+	if (!isNaN(params.renderY)) scaleY = parseFloat(params.renderY)/parseFloat(params.SVG.style('height'));
 
-	if (!isNaN(params.renderX) && !isNaN(params.renderY)){	
-		params.sizeScaler = params.renderX/params.targetWidth;
-		params.SVGscale = 1.;
-		params.controlsX = 0.;
+	trans = 'translate(0,0)scale(' + scaleX + ',' + scaleY + ')';
 
-		params.plotReady = false;
-		createPlot(params.renderX, params.renderY);
-
-		//wait until drawing in is complete
-		var imgCheck = setInterval(function(){ 
-			if (params.plotReady){
-				console.log('plot is ready')
-				clearInterval(imgCheck);
-				togglePlot();
-				setTimeout(function(){
-					saveImage();
-					params.SVGscale = saveSVGscale;
-					params.controlsX = saveControlsX;
-					params.sizeScaler = params.sizeScalerOrg;
-					setTimeout(function(){
-						resizePlot();
-					},1000)
-				}, (params.sortTransitionDuration + params.fadeTransitionDuration));
-			} 
-		}, 100);
-
-	} else {
-		console.log('bad image size ', params.renderX, params.renderY);
-	}
-}
-
-function saveImage(){
-	var svgString = getSVGString(params.SVG.node());
-
-	console.log('saving image ', params.renderX, params.renderY);
-	svgString2Image( svgString, params.renderX, params.renderY, 'png', save );
-
-	function save( dataBlob, filesize){
-		saveAs( dataBlob, params.filename ); // FileSaver.js function
-	}
-
-
-}
-
-
-// Below are the functions that handle actual exporting:
-// getSVGString ( svgNode ) and svgString2Image( svgString, width, height, format, callback )
-function getSVGString( svgNode ) {
-	svgNode.setAttribute('xlink', 'http://www.w3.org/1999/xlink');
-	var cssStyleText = getCSSStyles( svgNode );
-	appendCSS( cssStyleText, svgNode );
-
-	var serializer = new XMLSerializer();
-	var svgString = serializer.serializeToString(svgNode);
-	svgString = svgString.replace(/(\w+)?:?xlink=/g, 'xmlns:xlink='); // Fix root xlink without namespace
-	svgString = svgString.replace(/NS\d+:href/g, 'xlink:href'); // Safari NS namespace fix
-
-	return svgString;
-
-	function getCSSStyles( parentElement ) {
-		var selectorTextArr = [];
-
-		// Add Parent element Id and Classes to the list
-		selectorTextArr.push( '#'+parentElement.id );
-		for (var c = 0; c < parentElement.classList.length; c++)
-				if ( !contains('.'+parentElement.classList[c], selectorTextArr) )
-					selectorTextArr.push( '.'+parentElement.classList[c] );
-
-		// Add Children element Ids and Classes to the list
-		var nodes = parentElement.getElementsByTagName('*');
-		for (var i = 0; i < nodes.length; i++) {
-			var id = nodes[i].id;
-			if ( !contains('#'+id, selectorTextArr) )
-				selectorTextArr.push( '#'+id );
-
-			var classes = nodes[i].classList;
-			for (var c = 0; c < classes.length; c++)
-				if ( !contains('.'+classes[c], selectorTextArr) )
-					selectorTextArr.push( '.'+classes[c] );
-		}
-
-		// Extract CSS Rules
-		var extractedCSSText = '';
-		for (var i = 0; i < document.styleSheets.length; i++) {
-			var s = document.styleSheets[i];
-			
-			try {
-				if(!s.cssRules) continue;
-			} catch( e ) {
-					if(e.name !== 'SecurityError') throw e; // for Firefox
-					continue;
-				}
-
-			var cssRules = s.cssRules;
-			for (var r = 0; r < cssRules.length; r++) {
-				if ( contains( cssRules[r].selectorText, selectorTextArr ) )
-					extractedCSSText += cssRules[r].cssText;
-			}
-		}
-		
-
-		return extractedCSSText;
-
-		function contains(str,arr) {
-			return arr.indexOf( str ) === -1 ? false : true;
-		}
-
-	}
-
-	function appendCSS( cssText, element ) {
-		var styleElement = document.createElement('style');
-		styleElement.setAttribute('type','text/css'); 
-		styleElement.innerHTML = cssText;
-		var refNode = element.hasChildNodes() ? element.children[0] : null;
-		element.insertBefore( styleElement, refNode );
-	}
-}
-
-
-function svgString2Image( svgString, width, height, format, callback ) {
-	var format = format ? format : 'png';
-
-	var imgsrc = 'data:image/svg+xml;base64,'+ btoa( unescape( encodeURIComponent( svgString ) ) ); // Convert SVG string to data URL
-
-	var canvas = document.createElement('canvas');
-	var context = canvas.getContext('2d');
-
-	canvas.width = width;
-	canvas.height = height;
-
-	var image = new Image();
-	image.onload = function() {
-		context.clearRect ( 0, 0, width, height );
-		context.drawImage(image, 0, 0, width, height);
-
-		canvas.toBlob( function(blob) {
-			var filesize = Math.round( blob.length/1024 ) + ' KB';
-			if ( callback ) callback( blob, filesize );
-		});
-
-		
-	};
-
-	image.src = imgsrc;
+	saveSvgAsPng(params.SVG.node(), params.filename, {'width':width, 'height':height, 'transform':trans});
 
 }
